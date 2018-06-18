f242c1e8704513e3722b115f0186727cf654b7a8
Katerina Rubtsova <katerinarubtsova@mail.ru>
2018-06-18 16:32:00 +0300

    исправление опечаток, добавление файла в index.html

--- js/dialog.js
+++ js/dialog.js
@@ -3,54 +3,54 @@
 (function () {
   // Сделать диалог редактирования персонажа перетаскиваемым (draggable)
 
-var dialogHandle = setupDialog.querySelector('.setup-user-pic');
-var dialogHandler = setupDialog.querySelector('.upload');
+  var setup = document.querySelector('.overlay');
+  var dialogHandler = setup.querySelector('.upload');
 
-dialogHandler.addEventListener('mousedown', function (evt) {
-  evt.preventDefault();
+  dialogHandler.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
 
-  var startCoords = {
-    x: evt.clientX,
-    y: evt.clientY
-  };
-
-
-  var dragged = false;
-
-
-  var onMouseMove = function (moveEvt) {
-    moveEvt.preventDefault();
-
-    var shift = {
-      x: startCoords.x - moveEvt.clientX,
-      y: startCoords.y - moveEvt.clientY
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
     };
 
-    startCoords = {
-      x: moveEvt.clientX,
-      y: moveEvt.clientY
-    };
 
-    setupDialog.style.top = (setupDialog.offsetTop - shift.y) + 'px';
-    setupDialog.style.left = (setupDialog.offsetLeft - shift.x) + 'px';
-  };
+    var dragged = false;
 
-  var onMouseUp = function (upEvt) {
-    upEvt.preventDefault();
 
-    document.removeEventListener('mousemove', onMouseMove);
-    document.removeEventListener('mouseup', onMouseUp);
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
 
-    if (dragged) {
-      var onClickPreventDefault = function (evt) {
-        evt.preventDefault();
-        dialogHandler.removeEventListener('click', onClickPreventDefault)
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
       };
-      dialogHandler.addEventListener('click', onClickPreventDefault);
-    }
-  };
 
-  document.addEventListener('mousemove', onMouseMove);
-  document.addEventListener('mouseup', onMouseUp);
-});
-}) ();
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      setup.style.top = (setup.offsetTop - shift.y) + 'px';
+      setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+      if (dragged) {
+        var onClickPreventDefault = function (e) {
+          e.preventDefault();
+          dialogHandler.removeEventListener('click', onClickPreventDefault);
+        };
+        dialogHandler.addEventListener('click', onClickPreventDefault);
+      }
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+})();
